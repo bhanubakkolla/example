c52f5fb147769329f56920cc5ac786fb4106cc8e
Muthu Kumaravel Muthuraman <muthu.kumaravel@corp.ads.valuelabs.net>
2019-02-07 11:27:21 +0530

    k

---
+++ example-master/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projectF';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+        'thumbs-up',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/Octicons.svg'));
+  }
+}
